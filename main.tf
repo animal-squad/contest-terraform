0448101055c3d9b5b86fee4c0a5064d02bcef5d2
timepresent95 <timepresent95@gmail.com>
2024-12-10 23:14:24 +0900

    Feat: web 호스팅을 위한 ALB 추가

--- main.tf
+++ main.tf
@@ -30,10 +30,18 @@ module "network" {
   vpc_cidr_block = "10.0.0.0/20"
 
   public_subnet = {
-    web = {
+    web_0 = {
       az         = "ap-northeast-2a"
       cidr_block = cidrsubnet("10.0.0.0/20", 3, 0)
     }
+    web_1 = {
+      az         = "ap-northeast-2b"
+      cidr_block = cidrsubnet("10.0.0.0/20", 3, 1)
+    }
+    # redis = {
+    #   az         = "ap-northeast-2a"
+    #   cidr_block = cidrsubnet("10.0.0.0/20", 3, 1)
+    # }
   }
 
   enable_dns_hostnames = true
--- web.tf
+++ web.tf
@@ -13,9 +13,9 @@ module "security_group_web" {
       ip_protocol = "TCP"
       cidr_ipv4   = "0.0.0.0/0"
     }
-    http = {
-      from_port   = 80
-      to_port     = 80
+    nextjs = {
+      from_port   = 3000
+      to_port     = 3000
       ip_protocol = "TCP"
       cidr_ipv4   = "10.0.0.0/20"
     }
@@ -23,18 +23,26 @@ module "security_group_web" {
 }
 
 module "instance_web" {
+  for_each = {
+    web_0 = {
+      az = "ap-northeast-2a"
+    }
+    web_1 = {
+      az = "ap-northeast-2b"
+    }
+  }
 
   source  = "app.terraform.io/animal-squad/ec2/aws"
   version = "1.0.2"
 
-  name_prefix = "${local.name}-web-instance"
+  name_prefix = "${local.name}-instance-${each.key}"
 
   ami           = "ami-0d81776ee23e75c00" # Amazon Linux 2023 x86_64 with docker
-  az            = "ap-northeast-2a"
+  az            = each.value.az
   instance_type = "t3.small"
 
   security_group_ids          = [module.security_group_web.id]
-  subnet_id                   = module.network.public_subnets["web"].id
+  subnet_id                   = module.network.public_subnets[each.key].id
   vpc_id                      = module.network.vpc_id
   associate_public_ip_address = true
 
@@ -43,22 +51,33 @@ module "instance_web" {
   key_name = module.key_pair.name
 }
 
-# module "alb" {
-#   source  = "app.terraform.io/animal-squad/elb/aws"
-#   version = "1.0.0"
+module "web_alb" {
+  source  = "app.terraform.io/animal-squad/elb/aws"
+  version = "1.0.7"
 
-#   name = "${local.name}"
+  name = local.name
 
-#   certificate_arn = "arn:aws:acm:ap-northeast-2:015224529527:certificate/dc708e49-3eb0-4991-aded-20152719dc0b"
+  certificate_arn = "arn:aws:acm:ap-northeast-2:015224529527:certificate/dc708e49-3eb0-4991-aded-20152719dc0b"
 
-#   vpc_id = module.network.vpc_id
-#   subnet_ids #set(string)
+  vpc_id     = module.network.vpc_id
+  subnet_ids = [module.network.public_subnets["web_0"].id, module.network.public_subnets["web_1"].id]
 
-#   default_target_groups #map( object({ health_check_path = optional(string) port = number }) )
-#   default_targets #map( object({ target_group_key = string port = number })
+  default_target_groups = {
+    web = {
+      port = 80
+    }
+  }
 
-#   target_groups #map( object({ health_check_path = optional(string) port = number }) )
-#   targets #map( object({ target_group_key = string port = number }) )
-
-#   https_listener_rules #map(object({ path = list(string) host = list(string) priority = number health_check_path = optional(string) port = number }))
-# }
+  default_targets = {
+    web_0 = {
+      target_group_key = "web"
+      target_id        = module.instance_web.web_0.instance_id
+      port             = 3000
+    }
+    web_1 = {
+      target_group_key = "web"
+      target_id        = module.instance_web.web_1.instance_id
+      port             = 3000
+    }
+  }
+}
